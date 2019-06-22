d5f4e347bd5ab6af77abddeca4cebfa06e6c0e51
Muhammad <abdurazakov96@mail.ru>
2019-06-23 00:11:04 +0300

    Initial Commit

---
+++ HomeWorkFirst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWorkFirst
+//
+//  Created by Магомед Абдуразаков on 23/06/2019.
+//  Copyright © 2019 Магомед Абдуразаков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
